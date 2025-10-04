655e73f288931a37f79db02ed8c57c9e01831b9e
CostaValerio <valerio.costa30@gmail.com>
2025-10-04 01:45:42 +0100

    feat: Add additional pattern to exclude ISEQ objects in oei_env_sync_object_exclude table

--- sql/ddl/env_sync_capture/oei_env_sync_object_exclude.sql
+++ sql/ddl/env_sync_capture/oei_env_sync_object_exclude.sql
@@ -12,6 +12,7 @@ comment on column oei_env_sync_object_exclude.object_type is 'Oracle object type
 merge into oei_env_sync_object_exclude t
 using (
   select cast(null as varchar2(30)) object_type, 'BIN$%' name_like from dual union all
+  select cast(null as varchar2(30)) object_type, 'ISEQ%' name_like from dual union all
   select 'INDEX',   'SYS_%' from dual union all
   select 'TRIGGER', 'SYS_%' from dual
 ) s
